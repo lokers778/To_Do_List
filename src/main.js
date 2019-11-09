eb2f6e625186d9451d0a1d04743c09d1d7a2721d
Łukasz <lokers778@gmail.com>
2019-11-09 22:51:50 +0100

    added show / hidden new task list mechanic, basic style

--- src/main.js
+++ src/main.js
@@ -1,56 +1,72 @@
 class Task {
-    constructor(name, description, color) {
+    constructor(name, description) {
         this.name = name;
         this.description = description;
-        this.color=color;
+        this.tasks=[];
     }
-    newTaskFunction(){
-        const newTask=document.createElement("li");
-        const newTitle=document.createElement("h3");
-        const newDescription=document.createElement("p");
-        const removeTask=document.createElement("button");
-        newTitle.innerText=this.name;
-        newDescription.innerText=this.description;
+
+    newTaskFunction() {
+        const newTask = document.createElement("li");
+        const newTitle = document.createElement("h3");
+        const newDescription = document.createElement("p");
+        const removeTask = document.createElement("button");
+        newTitle.innerText = this.name;
+        newDescription.innerText = this.description;
         newTask.appendChild(newTitle);
         newTask.appendChild(newDescription);
-        newTask.appendChild(removeTask)
-        newTask.style.backgroundColor=this.color;
-        removeTask.addEventListener("click",(e)=>{
-         e.target.parentElement.parentElement.removeChild(e.target.parentElement)
-        })
+        newTask.appendChild(removeTask);
+        removeTask.innerText="Remove List"
+        newTask.classList.add("task")
+        removeTask.addEventListener("click", (e) => {
+            e.target.parentElement.parentElement.removeChild(e.target.parentElement)
+        });
         return newTask;
     }
 }
-const formInputs={
-    newTaskButton:document.querySelector("button[type='submit']"),
-    title:document.querySelector("input[type='text']"),
-    bgColor:document.querySelector("input[type='color']"),
-    importantRange:document.querySelector("select"),
-    description:document.querySelector("textarea"),
+
+const formInputs = {
+    form:document.querySelector("form"),
+    newTaskButton: document.querySelector("button[type='submit']"),
+    title: document.querySelector("input[type='text']"),
+    importantRange: document.querySelector("select"),
+    description: document.querySelector("textarea"),
 };
 
-formInputs.newTaskButton.addEventListener("click",(e)=>{
+formInputs.newTaskButton.addEventListener("click", (e) => {
     e.preventDefault()
-    const {title,bgColor,importantRange,description}=formInputs;
-  const Pop= new Task(title.value,description.value,bgColor.value);
-switch(importantRange.value){
-    case "1": {
-        const list = document.querySelector("div.task_list > .important");
-        const newListElement = Pop.newTaskFunction();
-        list.appendChild(newListElement);
-        break;
+    const {title, importantRange, description,form} = formInputs;
+    const Pop = new Task(title.value, description.value);
+    switch (importantRange.value) {
+        case "1": {
+            const list = document.querySelector(" .important");
+            const newListElement = Pop.newTaskFunction();
+            list.appendChild(newListElement);
+            break;
+        }
+
+        case "2": {
+            const list = document.querySelector(" .normal");
+            const newListElement = Pop.newTaskFunction();
+            newListElement.classList.add("normalTask")
+            list.appendChild(newListElement);
+            break;
+        }
+        case "3": {
+            const list = document.querySelector(".less");
+            const newListElement = Pop.newTaskFunction();
+            newListElement.classList.add("lessTask")
+            list.appendChild(newListElement);
+            break;
+        }
     }
-    case "2": {
-        const list = document.querySelector("body > div > div.task_list > .normal");
-        const newListElement = Pop.newTaskFunction();
-        list.appendChild(newListElement);
-        break;
-    }
-    case "3": {
-        const list = document.querySelector("body > div > div.task_list > .less");
-        const newListElement = Pop.newTaskFunction();
-        list.appendChild(newListElement);
-        break;
-    }
-}
+    form.parentElement.classList.add("hiddenList");
+    const taskList= document.querySelector("div.task_list")
+    taskList.classList.add("fullWidth")
 });
+document.querySelector(".showBtn").addEventListener("click",()=>{
+    formInputs.form.parentElement.classList.toggle("hiddenList");
+    console.log(formInputs.form)
+    const taskList= document.querySelector("div.task_list")
+    taskList.classList.toggle("fullWidth")
+
+})
